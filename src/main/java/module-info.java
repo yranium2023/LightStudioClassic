c1d095d40846e064d11cfb0dcac0365dfeeebd40
yranium <2647169392@qq.com>
2023-11-30 17:18:59 +0800

    Create the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires io.vproxy.vfx;
+    requires io.vproxy.base;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires javafx.media;
+    requires java.logging;
+    requires java.desktop;
+    requires com.github.kwhat.jnativehook;
+    requires vjson;
+
+    opens org.example to javafx.fxml;
+//    exports org.example;
+}
