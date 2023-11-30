21a79a74e60774f683fe175637bfb727c9c59809
yranium <2647169392@qq.com>
2023-11-30 19:55:27 +0800

    代码规范

---
+++ src/main/java/org/example/Theme/ThemeSet.java
@@ -0,0 +1,14 @@
+package org.example.Theme;
+
+import io.vproxy.vfx.theme.impl.DarkTheme;
+
+/**
+ * @Description 该类用于设定程序的主题、字体、图标
+ * @author 吴鹄远
+ * @date 2023/11/30 19:14
+ */
+public class ThemeSet extends DarkTheme {
+
+
+
+}
