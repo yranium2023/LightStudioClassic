3a77183a787289bbcf71ae8e32f4553f5787d304
yranium <2647169392@qq.com>
2023-12-05 23:12:31 +0800

    Update ImageClipScene.java

--- src/main/java/org/example/Scene/ImageClipScene.java
+++ src/main/java/org/example/Scene/ImageClipScene.java
@@ -1,11 +1,13 @@
 package org.example.Scene;
 
 import io.vproxy.vfx.ui.scene.VSceneRole;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import org.example.ImageModification.ImageClip;
+import org.example.ImageTools.ImportImageResource;
 import org.example.StaticValues;
 
 /**
@@ -26,18 +28,19 @@ public class ImageClipScene extends SuperScene{
 
         //创建一个矩形，用来包裹ImagePane
         Rectangle ImagePaneRec=new Rectangle(0,0,ImagePane.getPrefWidth()-2,ImagePane.getPrefHeight()-2){{
-            setFill(Color.TRANSPARENT);
+            setFill(Color.WHITE);
             setStroke(Color.WHITE);
             setStrokeType(StrokeType.INSIDE);
             setLayoutX(ImagePane.getLayoutX());
             setLayoutY(ImagePane.getLayoutY());
         }};
-
-        if(StaticValues.editingImage!=null){
-            ImageClip.imageClip(StaticValues.editingImage,ImagePane);
-        }
-        getContentPane().getChildren().add(ImagePane);
         ImagePane.setClip(ImagePaneRec);
+
+//        if(StaticValues.editingImage!=null){
+//            ImageClip.imageClip(StaticValues.editingImage,ImagePane);
+//        }
+        ImagePane.getChildren().add(new ImageView(ImportImageResource.getInstance().getImage("image/icon.png")));
+        getContentPane().getChildren().add(ImagePane);
 
 
     }
