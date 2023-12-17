025987df6eca9c5f559c040037f353f903c2077c
yranium2023 <2647169392@qq.com>
2023-12-17 19:21:18 +0800

    重写了编辑界面底部scene的ui，但是无法绑定layoutY(未开放接口)

--- src/main/java/org/example/Scene/ImageEditScene.java
+++ src/main/java/org/example/Scene/ImageEditScene.java
@@ -29,7 +29,6 @@ import org.example.ImageTools.ImageTransfer;
 import org.example.ImageTools.ImportImageResource;
 import org.example.LSMain;
 import org.example.Obj.HSLColor;
-import org.example.Obj.HSLInfo;
 import org.example.Pane.ImagePane;
 import org.example.StaticValues;
 
@@ -63,19 +62,17 @@ public class ImageEditScene extends SuperScene{
         getNode().setPrefHeight(45);
         getNode().setLayoutY(20);
     }};
-    VScene scene = new VScene(VSceneRole.DRAWER_HORIZONTAL);
+    VScene bottomScene = new VScene(VSceneRole.DRAWER_HORIZONTAL);
 
     public static VScrollPane scrollEditFlowPane = new VScrollPane(ScrollDirection.HORIZONTAL) {{
-        getNode().setLayoutX(0);
-        getNode().setLayoutY(0);
-        getNode().setPrefWidth(1265);
+        getNode().prefWidthProperty().bind(LSMain.getStage().getInitialScene().getContentPane().widthProperty().add(-20));
         getNode().setPrefHeight(100);
     }};
 
     // 创建 FlowPane 用于放图片按钮
     public static HBox hEditBox = new HBox() {{
         setLayoutX(10);
-        setLayoutY(0);
+        setLayoutY(10);
         setPrefHeight(scrollEditFlowPane.getNode().getPrefHeight());
         setPrefWidth(scrollEditFlowPane.getNode().getPrefWidth());
         // 设置行间距
@@ -544,48 +541,55 @@ public class ImageEditScene extends SuperScene{
 
 
         //生成下方pane
-        var navigatePane = new FusionPane();{
-            getNode().setPrefHeight(30);
-            getNode().setPrefWidth(1200);
-        }
-        navigatePane.getNode().layoutYProperty().bind(LSMain.getStage().getInitialScene().getContentPane().heightProperty().add(-50));
-        getContentPane().getChildren().add(navigatePane.getNode());
-        FXUtils.observeWidthCenter(LSMain.getStage().getInitialScene().getContentPane(),navigatePane.getNode());
+        var bottomPane = new FusionPane(){{
+            getNode().setPrefHeight(50);
+            getNode().prefWidthProperty().bind(LSMain.getStage().getInitialScene().getContentPane().widthProperty().add(-60));
+            getNode().layoutYProperty().bind(LSMain.getStage().getInitialScene().getContentPane().heightProperty().add(-60));
+        }};
+        //设置下方pane居中
+        FXUtils.observeWidthCenter(LSMain.getStage().getInitialScene().getContentPane(),bottomPane.getNode());
+        getContentPane().getChildren().add(bottomPane.getNode());
         var fromBottomButton = new FusionImageButton(ImportImageResource.getInstance().getImage("image/upArrow.png")) {{
-            setPrefWidth(1300);
+            prefWidthProperty().bind(bottomPane.getNode().widthProperty().add(-30));
             setPrefHeight(30);
             getImageView().setFitHeight(20);
         }};
-        FXUtils.observeWidthCenter(navigatePane.getNode(),fromBottomButton);
+        //设置button居中
+        FXUtils.observeWidthCenter(bottomPane.getNode(),fromBottomButton);
         // 在初始化部分定义一个 Timeline 用于刷新添加按钮
         Timeline refreshTimeline = new Timeline();
         //初始化scene
-        scene.enableAutoContentWidthHeight();
-        scene.getNode().setPrefHeight(120);
-        scene.getNode().setBackground(new Background(new BackgroundFill(
+        bottomScene.enableAutoContentWidthHeight();
+        bottomScene.getNode().setPrefHeight(120);
+        bottomScene.getNode().setBackground(new Background(new BackgroundFill(
                 Theme.current().subSceneBackgroundColor(),
                 CornerRadii.EMPTY,
                 Insets.EMPTY
         )));
         //新建VScrollPane用于生成滑动窗口，并存放flowPane
-        FXUtils.observeWidthHeight(scene.getContentPane(), scrollEditFlowPane.getNode());
+        FXUtils.observeWidthHeightCenter(bottomScene.getContentPane(), scrollEditFlowPane.getNode());
         //绑定两个pane的宽和高
         FXUtils.observeWidthHeight(scrollEditFlowPane.getNode(), hEditBox);
         // 创建一个矩形用于显示flowPane的边框
-        Rectangle flowPaneRec = new Rectangle(scrollEditFlowPane.getNode().getLayoutX() , scrollEditFlowPane.getNode().getLayoutY() , hEditBox.getPrefWidth() , hEditBox.getPrefHeight() ) {{
+        Rectangle flowPaneRec = new Rectangle( hEditBox.getPrefWidth() , hEditBox.getPrefHeight() ) {{
             setFill(Color.TRANSPARENT);
             setStroke(Color.WHITE); // 设置矩形的边框颜色
             setStrokeType(StrokeType.INSIDE);//边框为内嵌式，不会超出pane的范围
         }};
         flowPaneRec.heightProperty().bind(scrollEditFlowPane.getNode().heightProperty());
-        scene.getContentPane().getChildren().add(flowPaneRec);
-        scene.getContentPane().getChildren().add(scrollEditFlowPane.getNode());
+        flowPaneRec.widthProperty().bind(scrollEditFlowPane.getNode().widthProperty().add(10));
+        flowPaneRec.layoutXProperty().bind(scrollEditFlowPane.getNode().layoutXProperty().add(-5));
+        flowPaneRec.layoutYProperty().bind(scrollEditFlowPane.getNode().layoutYProperty());
+        bottomScene.getContentPane().getChildren().add(flowPaneRec);
+        bottomScene.getContentPane().getChildren().add(scrollEditFlowPane.getNode());
         scrollEditFlowPane.setContent(hEditBox);
+        FXUtils.observeWidthCenter(bottomScene.getContentPane(),scrollEditFlowPane.getNode());
+
         fromBottomButton.setOnAction(e -> {
-            if (!sceneGroupSup.get().getScenes().contains(scene)) {
-                sceneGroupSup.get().addScene(scene, VSceneHideMethod.TO_BOTTOM);
+            if (!sceneGroupSup.get().getScenes().contains(bottomScene)) {
+                sceneGroupSup.get().addScene(bottomScene, VSceneHideMethod.TO_BOTTOM);
             }
-             sceneGroupSup.get().show(scene, VSceneShowMethod.FROM_BOTTOM);
+             sceneGroupSup.get().show(bottomScene, VSceneShowMethod.FROM_BOTTOM);
             // 启动或重新开始 Timeline 定时器
             refreshTimeline.stop();  // 停止之前的定时器，以免叠加
             //50ms刷新一次
@@ -604,7 +608,7 @@ public class ImageEditScene extends SuperScene{
             refreshTimeline.setCycleCount(Timeline.INDEFINITE);
             refreshTimeline.play();
         });
-        navigatePane.getContentPane().getChildren().add(fromBottomButton);
+        bottomPane.getContentPane().getChildren().add(fromBottomButton);
     }
 
     public static void initEditImagePane(){
